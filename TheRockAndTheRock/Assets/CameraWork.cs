2f35113cb66f4a5ddd87a44567839d00375f3fe3
Kevin Roark <ker2143@columbia.edu>
2016-06-10 02:51:44 -0400

    swapping between two cameras

---
+++ TheRockAndTheRock/Assets/CameraWork.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraWork : MonoBehaviour {
+
+	public Camera dwayneCamera;
+	public Camera rockCamera;
+
+	bool isCameraOnDwayne = true;
+
+	void Update () {
+		// haha literally any input
+		if (Input.anyKeyDown) {
+			this.SwapCameras();
+		}
+	}
+
+	void SwapCameras () {
+		isCameraOnDwayne = !isCameraOnDwayne;
+
+		dwayneCamera.enabled = isCameraOnDwayne;
+		rockCamera.enabled = !isCameraOnDwayne;
+	}
+}
