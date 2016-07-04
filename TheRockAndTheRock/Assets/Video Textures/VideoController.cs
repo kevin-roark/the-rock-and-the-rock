ba02eb12e11b6a89a36eccbe184fc9c87fd22657
Kevin Roark <ker2143@columbia.edu>
2016-07-04 00:22:59 -0500

    TV

---
+++ TheRockAndTheRock/Assets/Video Textures/VideoController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+
+public class VideoController : MonoBehaviour {
+
+	public bool playAudio = true;
+
+	MovieTexture movie;
+	AudioSource movieAudio;
+
+	void Start () {
+		movie = GetComponent<Renderer>().material.mainTexture as MovieTexture;
+		if (!movie.isPlaying) {
+			movie.Play();
+		}
+
+		if (playAudio) {
+			movieAudio = GetComponent<AudioSource>();
+			movieAudio.clip = movie.audioClip;
+			if (!movieAudio.isPlaying) {
+				movieAudio.Play();
+			}
+		}
+	}
+
+	void Update () {
+
+	}
+}
